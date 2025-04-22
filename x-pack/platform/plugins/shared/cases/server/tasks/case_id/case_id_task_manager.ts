4641eec2a566c8e865a8931abafbef99bb297751
Jan Monschke <jan.monschke@elastic.co>
2025-04-22 13:36:45 +0200

    remove task state

--- x-pack/platform/plugins/shared/cases/server/tasks/case_id/case_id_task_manager.ts
+++ x-pack/platform/plugins/shared/cases/server/tasks/case_id/case_id_task_manager.ts
@@ -6,12 +6,8 @@
  */
 import { SavedObjectsClient, type CoreStart, type Logger } from '@kbn/core/server';
 import type { TaskInstance, TaskManagerStartContract } from '@kbn/task-manager-plugin/server';
-import type { TaskInstanceWithId } from '@kbn/task-manager-plugin/server/task';
-import { DEFAULT_SPACE_ID } from '@kbn/spaces-plugin/common';
 import type { CasesServerSetupDependencies } from '../../types';
 import { CASE_SAVED_OBJECT, CASE_ID_INCREMENTER_SAVED_OBJECT } from '../../../common/constants';
-import type { CasesIncrementIdTaskState, CasesIncrementIdTaskStateSchemaV1 } from './state';
-import { casesIncrementIdStateSchemaByVersion } from './case_id_task_state';
 import { CasesIncrementalIdService } from '../../services/incremental_id';
 
 export const CASES_INCREMENTAL_ID_SYNC_TASK_TYPE = 'cases_incremental_id_assignment';
@@ -22,9 +18,6 @@ const CASES_INCREMENTAL_ID_SYNC_INTERVAL_DEFAULT = '1m';
 
 export class CasesIdIncrementerTaskManager {
   private logger: Logger;
-  private namespaces: Set<string> = new Set([DEFAULT_SPACE_ID]);
-  private taskManager?: TaskManagerStartContract;
-  private taskInstance?: TaskInstanceWithId;
   private casesIncrementService?: CasesIncrementalIdService;
 
   constructor(plugins: CasesServerSetupDependencies, logger: Logger) {
@@ -36,9 +29,9 @@ export class CasesIdIncrementerTaskManager {
         [CASES_INCREMENTAL_ID_SYNC_TASK_TYPE]: {
           title: 'Cases Numerical ID assignment',
           description: 'Applying incremental numeric ids to cases',
-
-          stateSchemaByVersion: casesIncrementIdStateSchemaByVersion,
-          createTaskRunner: ({ taskInstance }) => {
+          // In order to ensure sequential id assignments, there can only every be one task running at a time
+          maxConcurrency: 1,
+          createTaskRunner: () => {
             return {
               run: async () => {
                 const initializedTime = new Date().toISOString();
@@ -48,47 +41,24 @@ export class CasesIdIncrementerTaskManager {
                   this.logger.error('Missing increment service necessary for task');
                   return undefined;
                 }
-                this.taskInstance = taskInstance;
-                const currentState = this.taskInstance.state as CasesIncrementIdTaskStateSchemaV1;
 
-                // For potential telemetry purposes
-                let initialCasesCountLackingId = 0;
-
-                // const lastIdByNameSpace: Record<string, number> =
-                //   currentState.last_update.last_id_by_namespace ?? {};
-
+                // Fetch all cases without an incremental id
                 const casesWithoutIncrementalIdResponse =
                   await this.casesIncrementService.getCasesWithoutIncrementalId();
-
-                initialCasesCountLackingId =
-                  initialCasesCountLackingId + casesWithoutIncrementalIdResponse.total;
-
                 const { saved_objects: casesWithoutIncrementalId } =
                   casesWithoutIncrementalIdResponse;
 
+                // Increment the case ids
                 await this.casesIncrementService.incrementCaseIds(casesWithoutIncrementalId);
 
                 const endTime = performance.now();
                 this.logger.info(`Increment id task ended at: ${new Date().toISOString()}`);
 
                 this.logger.debug(
-                  `Completed ${CASES_INCREMENTAL_ID_SYNC_TASK_ID}. Task run took ${
+                  `Task terminated ${CASES_INCREMENTAL_ID_SYNC_TASK_ID}. Task run took ${
                     endTime - startTime
                   }ms [ started: ${initializedTime} ]`
                 );
-
-                const newState = structuredClone(currentState);
-                return {
-                  state: {
-                    ...newState,
-                    namespaces: Array.from(this.namespaces),
-                    last_update: {
-                      timestamp: startTime,
-                      unincremented_cases_count: initialCasesCountLackingId,
-                      conflict_retry_count: 0,
-                    },
-                  },
-                };
               },
               cancel: async () => {
                 this.logger.info(`${CASES_INCREMENTAL_ID_SYNC_TASK_ID} task run was canceled`);
@@ -97,34 +67,6 @@ export class CasesIdIncrementerTaskManager {
           },
         },
       });
-    }
-  }
-
-  private updateTaskState(newState: Partial<CasesIncrementIdTaskStateSchemaV1>) {
-    if (this.taskManager) {
-      this.taskManager?.bulkUpdateState([CASES_INCREMENTAL_ID_SYNC_TASK_ID], (_state) => {
-        const clonedState = structuredClone(this.taskInstance?.state) ?? {};
-        return {
-          ...clonedState,
-          ...newState,
-        };
-      });
-    }
-  }
-
-  public addNamespace(namespace?: string) {
-    if (namespace) {
-      this.namespaces.add(namespace);
-      const newNamespaces = Array.from(this.namespaces);
-      this.updateTaskState({ namespaces: newNamespaces });
-    }
-  }
-
-  public removeNamespace(namespace?: string) {
-    if (namespace && this.namespaces.has(namespace)) {
-      this.namespaces.delete(namespace);
-      const newNamespaces = Array.from(this.namespaces);
-      this.updateTaskState({ namespaces: newNamespaces });
     }
   }
 
@@ -144,7 +86,6 @@ export class CasesIdIncrementerTaskManager {
       // await taskManager.removeIfExists(CASES_INCREMENTAL_ID_SYNC_TASK_ID);
 
       // Instantiate saved objects client
-      // This is great, because it's the internal SO client?
       const internalSavedObjectsRepository = core.savedObjects.createInternalRepository([
         CASE_SAVED_OBJECT,
         CASE_ID_INCREMENTER_SAVED_OBJECT,
@@ -156,26 +97,6 @@ export class CasesIdIncrementerTaskManager {
         this.logger
       );
 
-      const casesWithoutIncrementalId =
-        await this.casesIncrementService.getCasesWithoutIncrementalId({
-          perPage: 1,
-        });
-
-      const initializedTime = new Date().getTime();
-
-      const initialTaskState: CasesIncrementIdTaskState = {
-        namespaces: Array.from(this.namespaces),
-        on_initialization: {
-          timestamp: initializedTime,
-          unincremented_cases_count: casesWithoutIncrementalId.total,
-        },
-        last_update: {
-          timestamp: initializedTime,
-          unincremented_cases_count: casesWithoutIncrementalId.total,
-          conflict_retry_count: 0,
-        },
-      };
-
       const taskInstance = await taskManager.ensureScheduled({
         id: CASES_INCREMENTAL_ID_SYNC_TASK_ID,
         taskType: CASES_INCREMENTAL_ID_SYNC_TASK_TYPE,
@@ -183,12 +104,10 @@ export class CasesIdIncrementerTaskManager {
           interval: CASES_INCREMENTAL_ID_SYNC_INTERVAL_DEFAULT,
         },
         params: {},
-        state: initialTaskState,
+        state: {},
         scope: ['cases'],
       });
 
-      this.taskInstance = taskInstance;
-      this.taskManager = taskManager;
       this.logger.info(
         `${CASES_INCREMENTAL_ID_SYNC_TASK_ID} scheduled with interval ${taskInstance.schedule?.interval}`
       );
